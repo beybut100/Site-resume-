31e8e48539d774b7917eab5859f36f12b83bfc71
beybut100 <beybut27@gmail.com>
2016-11-01 22:23:45 +0200

    Error

--- App/Js/Controllers/menu-controller-example.js
+++ App/Js/Controllers/menu-controller-example.js
@@ -1,5 +1,8 @@
 
-
 myApp.controller("phonesController", function ($scope) {
-    
+    $scope.select = function(event) {
+		$(".active.active-color").removeClass("active active-color").addClass("standart-link");
+		$(event.target).parent().removeClass("standart-link").addClass("active active-color");
+	}
 });
+
