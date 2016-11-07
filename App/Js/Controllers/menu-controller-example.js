b60655713a915f490ce58866185a32ea7672df41
beybut100 <beybut27@gmail.com>
2016-11-07 21:51:46 +0200

    message

--- App/Js/Controllers/menu-controller-example.js
+++ App/Js/Controllers/menu-controller-example.js
@@ -9,8 +9,14 @@ myApp.controller("phonesController", function ($scope) {
 myApp.controller("NumberPhoneController", function ($scope) {
     $scope.show = function(event) {
 		$(".contact-form ").removeClass("hide-form");
-		
 	}
+	 $scope.hide = function(event) {
+
+	 
+		$(".contact-form ").removeClass("show-form").addClass("hide-form")	
+	}
+
+
 });
 
 
