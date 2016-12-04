d1bf16aade24d5e71a458814b42aa542458e5ebe
beybut100 <beybut27@gmail.com>
2016-12-04 19:40:23 +0200

    new change

--- App/Js/Controllers/menu-controller-example.js
+++ App/Js/Controllers/menu-controller-example.js
@@ -21,7 +21,43 @@ myApp.controller("NumberPhoneController", function ($scope) {
 
 
 myApp.controller("OnlyKirilicSymbol", function ($scope) {
-	$scope.change =  function(event, comment) { var pattern = new RegExp("\w+"); var result = pattern.test(comment); if (!result) {event.preventDefault(); }}
+ $scope.checkComment =  function(event) { 
+  var pattern = new RegExp("[а-яА-ЯЇїІіЄє0-9.,!?()-]"); 
+ 
+  if (event.key === " " || event.key === "Backspace") {
+   return;
+  }
+  
+  var result = pattern.test(event.key); 
+  console.log('pattern test result', event);
+  if (!result) {
+   event.preventDefault();
+  }
+ }
+});
 
-	}
+myApp.controller("LogoController", function ($scope) { 
+ 
+ $scope.changecolor = function (event) {
+  var that = this;
+  var randomcolor = $scope.colors[this.randomindex()]; 
+  console.log(event);
+  var target = $(event.target);
+  target.css("background-color", randomcolor.color); 
+  setTimeout(function(){that.changecolor(event);}, randomcolor.timeout);
+ }
+ 
+ $scope.colors = [
+  {id: 1, color: "red", timeout: 1500 },
+  {id: 2, color: "green", timeout: 300 },
+  {id: 3, color: "blue", timeout: 800 },
+  {id: 4, color: "pink", timeout: 450 }
+ ];
+ 
+ $scope.randomindex = function() {  
+  var min = 0;
+  var max = $scope.colors.length - 1;
+  var randomindexvalue = min + Math.floor(Math.random() * (max + 1 - min));
+  return randomindexvalue;
+ }
 });
