606659d0fb983a79738517990cec3275531e1b2f
beybut100 <beybut27@gmail.com>
2017-02-08 22:33:25 +0200

    new

--- App/Js/myApp.js
+++ App/Js/myApp.js
@@ -1,2 +1,18 @@
-var myApp = angular.module('myApp', []);
-var myApp = angular.module('myApp', ['chart.js']);
+
+var myApp = angular.module('myApp', ['chart.js' , 'ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+    templateUrl : "index.html"
+  })
+  .when("/about-me", {
+    templateUrl : "about-me.html"
+  })
+  .when("/quiz", {
+    templateUrl : "quiz.html"
+  })
+  .when("/kontcts", {
+    templateUrl : "html-kontacts.html"
+  });
+});
