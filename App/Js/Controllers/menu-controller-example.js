ba0d77eb05e47d7b9dc5a391f2492750d25e1a39
beybut100 <beybut27@gmail.com>
2017-02-17 21:45:36 +0200

    new

--- App/Js/Controllers/menu-controller-example.js
+++ App/Js/Controllers/menu-controller-example.js
@@ -257,13 +257,69 @@ $scope.CorrectImage = "";
 });  
 
 
-myApp.controller("TimeLineController", function ($scope) {
-  $scope.moviewheel = function ($event) {
-   dragObject.downX = $event.pageX;
-  dragObject.downY = $event.pageY; 
 
-   }
-}
+var sliderElem = document.getElementsByClassName("wheel")
+    var thumbElem = document.getElementsByClassName("line-one");
+
+    thumbElem.onmousedown = function(e) {
+      var thumbCoords = getCoords(thumbElem);
+      var shiftX = e.pageX - thumbCoords.left;
+      // shiftY здесь не нужен, слайдер двигается только по горизонтали
+
+      var sliderCoords = getCoords(sliderElem);
+
+      document.onmousemove = function(e) {
+        //  вычесть координату родителя, т.к. position: relative
+        var newLeft = e.pageX - shiftX - sliderCoords.left;
+
+        // курсор ушёл вне слайдера
+        if (newLeft < 0) {
+          newLeft = 0;
+        }
+        var rightEdge = sliderElem.offsetWidth - thumbElem.offsetWidth;
+        if (newLeft > rightEdge) {
+          newLeft = rightEdge;
+        }
+
+        thumbElem.style.left = newLeft + 'px';
+      }
+
+      document.onmouseup = function() {
+        document.onmousemove = document.onmouseup = null;
+      };
+
+      return false; // disable selection start (cursor change)
+    };
+
+    thumbElem.ondragstart = function() {
+      return false;
+    };
+
+    function getCoords(elem) { // кроме IE8-
+      var box = elem.getBoundingClientRect();
+
+      return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset
+      };
+
+    }
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
